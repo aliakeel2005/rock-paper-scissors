6e4989b777795b22ccd744eb64c782f923fe919e
aliakeel2005 <ali.akeelqq123@gmail.com>
2023-11-11 14:57:22 +0300

    gpt branch 1

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ function getComputerChoice() {
      }
      return x
 };
+let playerScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
      playerSelection = prompt("enter your choice", )
+     computerSelection = getComputerChoice();
+
+     
      /* rock */
     if(playerSelection === "rock" && computerSelection === "rock") {
         return "Draw! rock and rock"
@@ -35,24 +40,54 @@ function playRound(playerSelection, computerSelection) {
       } else if (playerSelection === "scissors" && computerSelection === "paper") {
          return "You win! scissors beat paper"
       } 
+      const result = determineWinner(playerSelection, computerSelection);
+      if (result.includes("You win!")) {playerScore++;} else if (result.includes("You lose!")) {computerScore++;}
+      console.log(result);
+      return result
+      function determineWinner(playerSelection, computerSelection) {
+         if(playerSelection === computerSelection) {
+            return "Draw!"
+          } else if (
+            (playerSelection === "rock" && computerSelection === "scissors") 
+            || (playerSelection === "paper" && computerSelection === "rock") 
+            ||(playerSelection === "scissors" && computerSelection === "paper"))
+            {return "You win! " + playerSelection + " beats " + computerSelection}
+            else {"You lose! " + computerScore + " beats " + playerSelection}}
+
 } 
-const playerSelection = ""
-   const computerSelection = getComputerChoice()
+/*
+function determineWinner(playerSelection, computerSelection) {
+   if(playerSelection === "rock" && computerSelection === "rock") {
+      return "Draw! rock and rock"
+    } else if(playerSelection === "rock" && computerSelection === "paper") {
+      return "You lose! paper beats rock"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+       return "You win! rock beats scissors" 
+    } 
+    /*paper
+    else if (playerSelection === "paper" && computerSelection === "paper") {
+       return "Draw! paper and paper"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+       return "You win! paper beats rock"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+       return "You lose! scissors beat paper"
+    } 
+    /*scissors
+    else if (playerSelection === "scissors" && computerSelection === "scissors") {
+       return "Draw! scissors and scissors"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+       return "You lose! rock beats scissors"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+       return "You win! scissors beat paper"
+    } 
+    return result
+}*/
+
 function game() { 
-   console.log(playRound(playerSelection, computerSelection));
-   console.log(playRound(playerSelection, computerSelection));
-   console.log(playRound(playerSelection, computerSelection));
-   console.log(playRound(playerSelection, computerSelection));
-   console.log(playRound(playerSelection, computerSelection));
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
+   console.log(playRound());
 }
-console.log(game())
-/*const playerSelection = "";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));*/
-
-
-
-/* get user input through prompt*/
-   
-
-
+(game())
